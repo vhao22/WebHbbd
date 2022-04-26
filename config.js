76aeb3c87dc040d6d8201bead8f6dff27f718b58
Nguyen Vuong Hao <vuonghao14298@gmail.com>
2022-04-26 14:32:35 +0700

    commit 1

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 20-11';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'download.jpg';
+const base64 = 'hot-girl.png';
+const giftImageBase64 = "data:image/png;base64, " + base64;
+const imgAB='hot-girl.png';
