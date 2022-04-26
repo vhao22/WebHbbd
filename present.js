5cd293c587e11fa8a5d4bfec1f8588a6e94962f9
Nguyen Vuong Hao <vuonghao14298@gmail.com>
2022-04-26 15:24:30 +0700

    ad

--- present.js
+++ present.js
@@ -2,7 +2,7 @@
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-
+//////
 var to = nameGirl;
 var gift_url = giftUrl;
 var gift_image_url = giftImage || giftImageBase64;
