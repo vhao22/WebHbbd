b8d58fd68034767b2275650fa37297563305b439
Nguyen Vuong Hao <vuonghao14298@gmail.com>
2022-06-21 23:31:24 +0700

    update 3

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const nameGirl = 'Bạn Mẫu Giáo';
 const giftUrl = '';
 const eventName = 'Mừng Sinh Nhật Muộn';
-const titleCard = 'Bạn Mẫu Giáo';
+const titleCard = 'Chúc Sinh Nhật Hơi Muôn';
 const contentCard = 'Chúc bạn mẫu giáo hết bị chê ốm &#128514;';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
