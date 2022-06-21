23493deb9c3c44b1abca8c7df96ed47fa58fbfd3
Nguyen Vuong Hao <vuonghao14298@gmail.com>
2022-06-21 23:36:37 +0700

    finish

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const contentCard = 'Chúc bạn mẫu giáo hết bị chê ốm &#128514;';
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'download.jpg';
+const giftImage = '2.jpg';
 const base64 = 'hot-girl.png';
 const giftImageBase64 = "data:image/png;base64, " + base64;
 const imgAB='1.jpg';
